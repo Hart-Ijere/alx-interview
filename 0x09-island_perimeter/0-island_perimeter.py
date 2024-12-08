033929e2a82703b3aecc406aa1ba3679847d4369
Hart-Ijere <iyeowayi@gmail.com>
2024-12-08 23:26:18 +0100

    Update island perimeter

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -4,6 +4,7 @@ This module provides a function to calculate the perimeter
 of an island represented in a grid.
 """
 
+
 def island_perimeter(grid):
     """
     Calculate the perimeter of the island described in grid.
@@ -18,9 +19,7 @@ def island_perimeter(grid):
     for i in range(len(grid)):
         for j in range(len(grid[i])):
             if grid[i][j] == 1:
-                # Add 4 for the land cell
                 perimeter += 4
-                # Subtract 2 for each adjacent land cell
                 if i > 0 and grid[i - 1][j] == 1:
                     perimeter -= 2
                 if j > 0 and grid[i][j - 1] == 1:
