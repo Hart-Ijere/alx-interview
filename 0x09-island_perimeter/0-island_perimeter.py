b50b84a95786ecb4ea1cb6c61ea6b19d990b63bb
Hart-Ijere <iyeowayi@gmail.com>
2024-12-08 23:11:34 +0100

    Return the perimeter of the island returnrd in a grid

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+This module provides a function to calculate the perimeter
+of an island represented in a grid.
+"""
+
+def island_perimeter(grid):
+    """
+    Calculate the perimeter of the island described in grid.
+
+    Args:
+        grid (list of list of int): The grid representing the island and water.
+
+    Returns:
+        int: The perimeter of the island.
+    """
+    perimeter = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j] == 1:
+                # Add 4 for the land cell
+                perimeter += 4
+                # Subtract 2 for each adjacent land cell
+                if i > 0 and grid[i - 1][j] == 1:
+                    perimeter -= 2
+                if j > 0 and grid[i][j - 1] == 1:
+                    perimeter -= 2
+    return perimeter
