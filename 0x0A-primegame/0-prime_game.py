a1e2aaaf6013475ec0efc5e752f7bee8a7b6e560
Hart-Ijere <iyeowayi@gmail.com>
2025-02-03 19:25:18 +0100

    Add prime game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,40 +1,51 @@
 #!/usr/bin/python3
+
+def sieve(n):
+    """Returns a list where prime[i] is True if i is a prime number, otherwise False."""
+    primes = [True] * (n + 1)
+    primes[0] = primes[1] = False  # 0 and 1 are not prime numbers
+    for i in range(2, int(n**0.5) + 1):
+        if primes[i]:
+            for j in range(i * i, n + 1, i):
+                primes[j] = False
+    return primes
+
+def count_primes(n, primes):
+    """Returns the number of prime numbers up to n."""
+    return sum(primes[:n + 1])
+
 def isWinner(x, nums):
-    """Determine the winner of a prime game."""
-    if x < 1 or not nums:
+    """
+    Determines the winner of the game.
+    
+    Parameters:
+    x (int): Number of rounds
+    nums (list): List containing values of n for each round
+    
+    Returns:
+    str: Name of the player with the most wins (Maria or Ben) or None if it's a tie.
+    """
+    if not nums or x < 1:
         return None
-
-    # Find the maximum value of n to generate primes
-    max_n = max(nums)
-
-    # Sieve of Eratosthenes to precompute primes up to max_n
-    is_prime = [True] * (max_n + 1)
-    is_prime[0] = is_prime[1] = False  # 0 and 1 are not primes
-    for i in range(2, int(max_n**0.5) + 1):
-        if is_prime[i]:
-            for j in range(i * i, max_n + 1, i):
-                is_prime[j] = False
-
-    # Precompute the number of primes up to each index
-    prime_counts = [0] * (max_n + 1)
-    for i in range(1, max_n + 1):
-        prime_counts[i] = prime_counts[i - 1] + (1 if is_prime[i] else 0)
+    
+    max_n = max(nums)  # Get the largest n in all rounds
+    primes = sieve(max_n)  # Precompute prime numbers up to max_n
 
     maria_wins = 0
     ben_wins = 0
 
-    # Simulate each game
     for n in nums:
-        # If the number of primes is odd, Maria wins; otherwise, Ben wins
-        if prime_counts[n] % 2 == 1:
+        prime_count = count_primes(n, primes)
+        
+        # If Maria has an odd number of moves, she wins, otherwise Ben wins
+        if prime_count % 2 == 1:
             maria_wins += 1
         else:
             ben_wins += 1
-
-    # Determine the overall winner
+    
     if maria_wins > ben_wins:
         return "Maria"
     elif ben_wins > maria_wins:
         return "Ben"
     else:
-        return None
+        return None  # Tie
